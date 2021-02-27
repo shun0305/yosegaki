b90fc61165cfe1121e7d9c04410a1fdd50386192
ChieARIYOSHI <chic.720ants@gmail.com>
2021-02-27 20:25:00 +0900

    day1_2024_ari

--- user_register_act.php
+++ user_register_act.php
@@ -9,12 +9,43 @@ include('functions.php');
 // データ受け取り
 $username = $_POST["username"];
 $password = $_POST["password"];
+// $icon    = $_POST["upfile"];
+
+// ここからファイルアップロード&DB登録の処理を追加しよう！！！
+
+if (!isset($_FILES['upfile']) && $_FILES['upfile']['error'] != 0) {
+  // 送られていない,エラーが発生,などの場合
+  exit('Error:画像が送信されていません');
+} else {
+  $uploaded_file_name = $_FILES['upfile']['name']; //ファイル名の取得
+  $temp_path = $_FILES['upfile']['tmp_name']; //tmpフォルダの場所
+  $directory_path = 'upload/'; //アップロード先フォルダ
+
+  $extension = pathinfo($uploaded_file_name, PATHINFO_EXTENSION);
+  // $unique_name = date('YmdHis')  . md5(session_id()) . "." . $extension;
+  $unique_name = $username . "_icon" . "." . $extension;
+  $filename_to_save = $directory_path . $unique_name;
+
+  // var_dump($temp_path);
+  // exit();
+
+  if (!is_uploaded_file($temp_path)) {
+    exit('Error:画像がありません'); // tmpフォルダにデータがない
+  } else { // ↓ここでtmpファイルを移動する
+    if (!move_uploaded_file($temp_path, $filename_to_save)) {
+      exit('Error:ードできませんでした'); // 画像の保存に失敗
+    } else {
+      chmod($filename_to_save, 0644); // 権限の変更
+      //$img = '<img src="' . $filename_to_save . '" >'; // imgタグを設定
+    }
+  }
+}
 
 // DB接続関数
 $pdo = connect_to_db();
 
 // ユーザ存在有無確認
-$sql = 'SELECT COUNT(*) FROM users_table WHERE username=:username';
+$sql = 'SELECT COUNT(*) FROM user_list_table WHERE user_name=:username';
 
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':username', $username, PDO::PARAM_STR);
@@ -37,12 +68,13 @@ if ($stmt->fetchColumn() > 0) {
 
 // ユーザ登録SQL作成
 // `created_at`と`updated_at`には実行時の`sysdate()`関数を用いて実行時の日時を入力する
-$sql = 'INSERT INTO users_table(id, username, password, is_admin, is_deleted, created_at, updated_at) VALUES(NULL, :username, :password, 0, 0, sysdate(), sysdate())';
+$sql = 'INSERT INTO user_list_table(id, user_id, user_name, password, nickname, icon, is_admin, created_at, updated_at) VALUES(NULL, 1, :username, :password, "nickname", :icon, 1, sysdate(), sysdate())';
 
 // SQL準備&実行
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':username', $username, PDO::PARAM_STR);
 $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+$stmt->bindValue(':icon', $unique_name, PDO::PARAM_STR);
 $status = $stmt->execute();
 
 // データ登録処理後
