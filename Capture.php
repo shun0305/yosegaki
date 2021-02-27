18bf2ba42d0ca92e20f01aecbd20776e76e00d13
shun0305 <shunbbra@icloud.com>
2021-02-28 00:53:28 +0900

    no message

--- Capture.php
+++ Capture.php
@@ -1,3 +1,32 @@
+<?php
+
+session_start();
+include("functions.php");
+// DB接続情報
+$pdo = connect_to_db();
+
+// 参照はSELECT文!
+$sql = 'SELECT * FROM send_user_table';
+$stmt = $pdo->prepare($sql);
+$status = $stmt->execute();
+
+if ($status == false) {
+  $error = $stmt->errorInfo();
+  exit('sqlError:' . $error[2]);
+} else {
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $output = "";
+  foreach ($result as $record) {
+    $output .= "<div class='west'>";
+    $output .= "<div class='test'>";
+    $output .= "<p>{$record["user_name"]}</p>";
+    $output .= "<p>{$record["message"]}</p>";
+    $output .= "</div>";
+    $output .= "</div>";
+  }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -7,6 +36,7 @@
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
   <link rel="stylesheet" href="css/captture.css">
+  <link rel="icon" href="img/icon.ico">
 </head>
 
 <body>
@@ -17,23 +47,15 @@
   </div>
 
   <h3>HTMLの範囲（ここが色紙になる id targetにの中に入れること）</h3>
-  <div id="target" style="background-color: pink; height: 550px;">
+  <div id="target" style=" height: 550px;">
     <!-- この中にHTML要素を入れると
             下のボタンを押したときにここの範囲が印刷されます
             横幅などはタグ内にwidthで設定してみてね -->
     <div class="boxContainer">
-      <div class="box">
-        <p>名前</p>
-        <P>テキスト</P>
-      </div>
-      <div class="box">
-        <p>名前</p>
-        <P>テキスト</P>
-      </div>
-      <div class="box">
-        <p>名前</p>
-        <P>テキスト</P>
-      </div>
+
+      <?= $output ?>
+
+
     </div>
     <h1 id="taro">タロ先生</h1>
     <div class="boxContainer">
@@ -71,11 +93,10 @@
 
   <button type="submit" class="button">上のHTMLを画像変換してプリンターに画像データを送るボタン</button>
 
-
   <script>
     const result = document.getElementById('result');
 
-    document.getElementsByClassName('button')[0].onclick = function () {
+    document.getElementsByClassName('button')[0].onclick = function() {
 
       const board = document.querySelector("#board");
       console.log(board);
@@ -86,21 +107,22 @@
       document.getElementById('div').appendChild(img_element);
       let data = result.src; // 渡したいデータ
       $.ajax({
-        type: "POST", //　GETでも可
-        url: "printSample.php", //　送り先
-        data: { 'src': data }, //　渡したいデータをオブジェクトで渡す
-        dataType: "json", //　データ形式を指定
-        scriptCharset: 'utf-8' //　文字コードを指定
-      })
+          type: "POST", //　GETでも可
+          url: "printSample.php", //　送り先
+          data: {
+            'src': data
+          }, //　渡したいデータをオブジェクトで渡す
+          dataType: "json", //　データ形式を指定
+          scriptCharset: 'utf-8' //　文字コードを指定
+        })
         .then(
-          function (param) {　 //　paramに処理後のデータが入って戻ってくる
+          function(param) { //　paramに処理後のデータが入って戻ってくる
             // console.log(param); //　帰ってきたら実行する処理
           },
-          function (XMLHttpRequest, textStatus, errorThrown) {
+          function(XMLHttpRequest, textStatus, errorThrown) {
             console.log(errorThrown); //　エラー表示
           });
     }
-
   </script>
 
 
@@ -121,11 +143,11 @@
 
   <script>
     //ロードされた際の処理として実施：
-    window.onload = function () {
+    window.onload = function() {
 
       //HTML内に画像を表示
       html2canvas(document.getElementById("target"), {
-        onrendered: function (canvas) {
+        onrendered: function(canvas) {
           //imgタグのsrcの中に、html2canvasがレンダリングした画像を指定する。
 
           var imgData = canvas.toDataURL();
